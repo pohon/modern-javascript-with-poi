f5715e727d32ef9306c7b4245b47a5161fc10319
Sullivan Osmond <osmond@tiket.com>
2017-12-18 15:46:35 +0700

    import styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import moment from 'moment';
+import './styles/styles.scss';
 
 const now = moment().format('dd MM YYYY');
 const tomorrow = moment().add(1, 'd').format('dd MM YYYY');
-const content = `<p>Hari ini: ${now}, besok: ${tomorrow}</p>`;
+const content = `<div class="box"><p>Hari ini: ${now}, besok: ${tomorrow}</p></div>`;
 
 document.getElementById('app').innerHTML = content;
